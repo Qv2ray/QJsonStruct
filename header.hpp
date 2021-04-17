d852f541386d800a51249311e5f5caaa97838611
QxQ <59914293+U-v-U@users.noreply.github.com>
2021-04-17 23:17:52 +0800

    update demo

--- header.hpp
+++ header.hpp
@@ -8,12 +8,10 @@ struct ChildObject : public QObject
 {
     Q_OBJECT
   public:
-    QJS_CONSTRUCTOR(ChildObject)
-
     QJS_PROP_D(QString, childString, "")
     QJS_PROP_D(int, childInt, 0, REQUIRED)
 
-    QJS_FUNCTION(F(childString, childInt))
+    QJS_FUNCTION(ChildObject, F(childString, childInt))
 };
 
 class myStruct : public QObject
@@ -22,13 +20,10 @@ class myStruct : public QObject
     typedef QMap<QString, QString> stringmap;
 
   public:
-    QJS_CONSTRUCTOR(myStruct)
-
     QJS_PROP_D(int, x, 0, REQUIRED)
     QJS_PROP_D(QString, string, "vstring", REQUIRED)
     QJS_PROP(QList<QString>, stringlist)
     QJS_PROP(stringmap, map)
     QJS_PROP(ChildObject, child)
-
-    QJS_FUNCTION(F(x, string, stringlist))
+    QJS_FUNCTION(myStruct, F(x, string, stringlist, map, child))
 };
